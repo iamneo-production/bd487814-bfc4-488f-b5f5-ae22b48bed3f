a5db9164e76679716e63fc6aa189010382b9c91a
Jayaraja-SK <jayaraja19041@cse.ssn.edu.in>
2022-03-16 10:55:53 +0000

    ADDING SPRING.

--- springapp/src/main/java/com/examly/springapp/Controller/SignupController.java
+++ springapp/src/main/java/com/examly/springapp/Controller/SignupController.java
@@ -9,17 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import com.examly.springapp.Service.UserService;
-import com.examly.springapp.Model.UserModel;
+//import com.examly.springapp.Service.UserService;
+//import com.examly.springapp.Model.UserModel;
 
 @RestController
 
 public class SignupController {
 	
-	@Autowired
-	private UserService userService;
+	/*@Autowired
+	private UserService userService;*/
 	
-	@RequestMapping(value="/signup",method=RequestMethod.POST)
+	/*@RequestMapping(value="/signup",method=RequestMethod.POST)
 	public ResponseEntity<Object> saveUser(@RequestBody UserModel user){
 		if(userService.newUserSignUp(user))
 		{
@@ -30,5 +30,10 @@ public class SignupController {
 		{
 			return new ResponseEntity<>(false,HttpStatus.BAD_REQUEST);
 		}
-	}
+	}*/
+
+    @RequestMapping(value="/")
+    public String Hello(){
+        return "Hello";
+    }
 }
