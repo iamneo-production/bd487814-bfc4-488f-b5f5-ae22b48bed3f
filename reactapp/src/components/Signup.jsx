5bc2e56f9f2226b8629b332c1d91393956a48238
prabhathkumar1729 <prabhathkumar1729@gmail.com>
2022-03-18 15:49:36 +0530

    added few register form fields and invisible scrollbar to signup page for FE

--- reactapp/src/components/Signup.jsx
+++ reactapp/src/components/Signup.jsx
@@ -1,8 +1,11 @@
-import React from "react";
-import { Form, FormGroup, Label, Input } from "reactstrap";
+import React, { useState } from "react";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 import bgImg from "../images/backgroundimg.jpg";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import "./scrollbar.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
 
 //Frombox to style the form
 const Formbox = styled.div`
@@ -20,10 +23,11 @@ const Formbox = styled.div`
   color: rgb(0, 0, 0);
   box-shadow: 0 0 10px;
   position: relative;
-  overflow: hidden;
+  overflow-y: scroll;
   font-family: Fredoka;
   backdrop-filter: blur(20px);
   background-color: rgba(255, 255, 255, 0.8);
+  scrollbar-width: 0px;
 `;
 
 // Container to style and hold Form and logo
@@ -45,10 +49,26 @@ const Formlogo = styled.div`
 `;
 
 function Signup(props) {
+  const [userFirstname, setFirstname] = useState("");
+  const [userLastname, setLastname] = useState("");
+  const [userid, setuserid] = useState("");
+  const [email, setemail] = useState("");
+  const [mobileNumber, setmobileNumber] = useState("");
+  const [password, setpassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (mobileNumber.length != 10) {
+      alert("Ivalid Phone number");
+    }
+  };
+
+  const notify = () => toast.success("Registered successfully!");
+
   return (
     <div id={props.id}>
       <Container>
-        <Formbox>
+        <Formbox className="scrollbar scrollbar-juicy-peach">
           <Formlogo>
             <img
               src={require("../images/PhotoFramed-logos_black.png")}
@@ -58,32 +78,60 @@ function Signup(props) {
               height={70}
             />
           </Formlogo>
-          <Form className="signup-form">
+          <Form>
             <h1 className="text-center">Register</h1>
             <FormGroup>
               <Label>First Name</Label>
               <Input
-                type="String"
+                type="text"
                 id="userFirstname"
                 placeholder="Enter First name"
+                value={userFirstname}
+                onChange={(e) => setFirstname(e.target.value)}
                 required
               />
             </FormGroup>
             <FormGroup>
               <Label>Last Name</Label>
               <Input
-                type="String"
+                type="text"
                 id="userLastname"
                 placeholder="Enter Last name"
+                value={userLastname}
+                onChange={(e) => setLastname(e.target.value)}
                 required
               />
             </FormGroup>
             <FormGroup>
               <Label>User ID</Label>
               <Input
-                type="String"
+                type="text"
                 id="userid"
                 placeholder="Enter username"
+                value={userid}
+                onChange={(e) => setuserid(e.target.value)}
+                required
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label>Email ID</Label>
+              <Input
+                type="email"
+                id="email"
+                placeholder="Enter email id"
+                value={email}
+                onChange={(e) => setemail(e.target.value)}
+                required
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label>Mobile number</Label>
+              <Input
+                type="number"
+                id="mobileNumber"
+                placeholder="Enter mobile number"
+                value={mobileNumber}
+                onChange={(e) => setmobileNumber(e.target.value)}
                 required
               />
             </FormGroup>
@@ -93,12 +141,18 @@ function Signup(props) {
                 type="password"
                 id="password"
                 placeholder="Enter password"
+                value={password}
+                onChange={(e) => setpassword(e.target.value)}
                 required
               />
             </FormGroup>
-            <button className="btn-lg btn-dark btn-block" id="submitButton">
+            <Button
+              className="btn-lg btn-dark btn-block"
+              id="submitButton"
+              onClick={notify}
+            >
               Register
-            </button>
+            </Button>
             <div className="text-center">
               {/* <a href="/signup">Existing user? Sign in</a> */}
               <Link to="/login">Existing user? Sign in</Link>
@@ -106,6 +160,7 @@ function Signup(props) {
           </Form>
         </Formbox>
       </Container>
+      <ToastContainer autoClose={2000} />
     </div>
   );
 }
