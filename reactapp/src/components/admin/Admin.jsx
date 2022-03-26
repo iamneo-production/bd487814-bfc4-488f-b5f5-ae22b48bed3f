ebe341eee53afcc1e35743e02c3ad51602fd8904
prabhathkumar1729 <prabhathkumar1729@gmail.com>
2022-03-26 20:04:10 +0530

    added users list to admin home page FE

--- reactapp/src/components/admin/Admin.jsx
+++ reactapp/src/components/admin/Admin.jsx
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import "./Admin.scss";
 import "./darkmode/dark.scss";
 import { DarkModeContext } from "./darkmode/DarkModeContext";
 import Navbar from "./Navbar";
-import { useContext } from "react";
 import Userslist from "./Userslist";
 
 //Home page at admin side
