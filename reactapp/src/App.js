3025dd8976f5a7227c72c927d1f0572e223ce83f
prabhathkumar1729 <prabhathkumar1729@gmail.com>
2022-03-30 14:36:41 +0530

    Successful navigation from signup page FE

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Signup } from "./components/Signup";
+import Signup from "./components/Signup";
 import Login  from "./components/Login";
 import Admin from "./components/admin/Admin";
 import Home from "./components/Home";
--- reactapp/src/components/Signup.jsx
+++ reactapp/src/components/Signup.jsx
@@ -1,14 +1,12 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 import bgImg from "../images/backgroundimg.jpg";
 import styled from "styled-components";
-import { Link, useNavigate, Navigate } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import "./scrollbar.css";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer, toast } from "react-toastify";
 import axios from "axios";
-
-
 
 //Frombox to style the form
 const Formbox = styled.div`
@@ -51,64 +49,30 @@ const Formlogo = styled.div`
   justify-content: center;
 `;
 
-export class Signup extends Component {
-  constructor() {
-    super();
-    this.state = {
-      userFirstname: "",
-      userLastname: "",
-      userid: "",
-      password: "",
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+const Signup = () => {
+  const [data, setData] = useState({
+    userFirstname: "",
+    userLastname: "",
+    userid: "",
+    password: "",
+  });
 
-  // adminnavigate = (e) => {
-  //   let navigate = useNavigate();
-  //   navigate("/admin"); 
-  // }
-
-  userFirstnamehandler = (event) => {
-    this.setState({
-      userFirstname: event.target.value,
-    });
+  const { userFirstname, userLastname, userid, password } = data;
+  const [valid, setValid] = useState(false);
+  const changeHandler = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
   };
 
-  userLastnamehandler = (event) => {
-    this.setState({
-      userLastname: event.target.value,
-    });
-  };
-
-  useridhandler = (event) => {
-    this.setState({
-      userid: event.target.value,
-    });
-  };
-
-  passwordhandler = (event) => {
-    this.setState({
-      password: event.target.value,
-    });
-  };
-
-  //Notification for signup
-  notify = (e) => toast(e);
-
-  handleSubmit = (event) => {
-    // <Link to="/admin "></Link>
-    // event.preventDefault();
-    //this.adminnavigate();
-
-    
-    let auth=false;
-    if (this.state.password.toString().length <= 8) {
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (password.toString().length <= 8) {
       alert(`Password must contain atleast 8 characters.`);
     } else if (
-      this.state.password.search(/[0-9]/) === -1 ||
-      this.state.password.search(/[a-z]/) === -1 ||
-      this.state.password.search(/[A-Z]/) === -1 ||
-      this.state.password.search(
+      password.search(/[0-9]/) === -1 ||
+      password.search(/[a-z]/) === -1 ||
+      password.search(/[A-Z]/) === -1 ||
+      //eslint-disable-next-line
+      password.search(
         /[!\@\#\$\^\&\*\(\)\+\=\-\/\?\.\,\>\<\}\{\]\[\'\"\;\:\]\}\{\`\~]/
       ) === -1
     ) {
@@ -116,168 +80,126 @@ export class Signup extends Component {
         `Password must contain atleast 1 number, 1 Uppercase, 1 Lowercase and 1 Special character.`
       );
     } else {
-      console.log(this.state);
-      
-      const user = {
-        userFirstname: this.state.userFirstname,
-        userLastname: this.state.userLastname,
-        userid: this.state.userid,
-        password: this.state.password,
-        
-      };
-      axios.post("http://localhost:8081/signup", user).then((res) => {
-        this.notify("Registered Successfully!");
+      console.log(data);
+      axios.post("http://localhost:8081/signup", data).then((res) => {
+        notify("Registered Successfully!");
         // res =>{auth=true}
-        this.if(res)
-        {
-          return <Link to="/login" />
+        if (res) {
+          setValid(true);
         }
       });
     }
-    event.preventDefault();
   };
 
-  // if(auth){
-  //   return <Navigate to="/login" />
-  // }
-  // const loginApi = () => {
-  //   fetch("http://localhost:3000/", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({
-  //       firstname: userFirstname,
-  //       lastname: userLastname,
-  //       email: email,
-  //       password: password,
-  //       mobileNumber: mobileNumber,
-  //       userid: userid,
-  //     }),
-  //   })
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       if (data.status === "success") {
-  //         toast.success("Signup Successful");
-  //         props.history.push("/login");
-  //       } else {
-  //         toast.error("Signup Failed");
-  //       }
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // };
+  if (valid) {
+    return <Navigate to="/login" />;
+  } else {
+    notify("Unsuccessful registration!");
+  }
 
-  // fetch("https://api.example.com/items")
-  //     .then(res => res.json())
-  //     .then(
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   if (mobileNumber.length != 10) {
-  //     alert("Ivalid Phone number");
-  //   }
-  // };
-  // const notify = () => toast.success("Registered successfully!");
-
-  render() {
-    return (
-      <div id="signupBox">
-        <Container>
-          <Formbox className="scrollbar scrollbar-juicy-peach">
-            <Formlogo>
-              <img
-                src={require("../images/PhotoFramed-logos_black.png")}
-                alt="Logo"
-                border-radius={100}
-                width={70}
-                height={70}
-              />
-            </Formlogo>
-            <Form className="signup-form" onSubmit={this.handleSubmit}>
-              <h1 className="text-center">Register</h1>
-              <FormGroup>
-                <Label>First Name</Label>
-                <Input
-                  type="text"
-                  id="userFirstname"
-                  placeholder="Enter First name"
-                  value={this.state.userFirstname}
-                  onChange={this.userFirstnamehandler}
-                  required
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label>Last Name</Label>
-                <Input
-                  type="text"
-                  id="userLastname"
-                  placeholder="Enter Last name"
-                  value={this.state.userLastname}
-                  onChange={this.userLastnamehandler}
-                  required
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label>User ID</Label>
-                <Input
-                  type="text"
-                  id="userid"
-                  placeholder="Enter username"
-                  value={this.state.userid}
-                  onChange={this.useridhandler}
-                  required
-                />
-              </FormGroup>
-              {/* <FormGroup>
-              <Label>Email ID</Label>
+  const notify = (e) => toast(e);
+  return (
+    <div id="signupBox">
+      <Container>
+        <Formbox className="scrollbar scrollbar-juicy-peach">
+          <Formlogo>
+            <img
+              src={require("../images/PhotoFramed-logos_black.png")}
+              alt="Logo"
+              border-radius={100}
+              width={70}
+              height={70}
+            />
+          </Formlogo>
+          <Form className="signup-form" onSubmit={submitHandler}>
+            <h1 className="text-center">Register</h1>
+            <FormGroup>
+              <Label>First Name</Label>
               <Input
-                type="email"
-                id="email"
-                placeholder="Enter email id"
-                value={email}
-                onChange={(e) => setemail(e.target.value)}
+                type="text"
+                id="userFirstname"
+                name="userFirstname"
+                placeholder="Enter First name"
+                value={userFirstname}
+                onChange={changeHandler}
                 required
               />
             </FormGroup>
             <FormGroup>
-              <Label>Mobile number</Label>
+              <Label>Last Name</Label>
               <Input
-                type="number"
-                id="mobileNumber"
-                placeholder="Enter mobile number"
-                value={mobileNumber}
-                onChange={(e) => setmobileNumber(e.target.value)}
+                type="text"
+                id="userLastname"
+                name="userLastname"
+                placeholder="Enter Last name"
+                value={userLastname}
+                onChange={changeHandler}
                 required
               />
-            </FormGroup> */}
-              <FormGroup>
-                <Label>Password</Label>
-                <Input
-                  type="password"
-                  id="password"
-                  placeholder="Enter password"
-                  value={this.state.password}
-                  onChange={this.passwordhandler}
-                  required
-                />
-              </FormGroup>
-              <Button
-                className="btn-lg btn-dark btn-block"
-                id="submitButton"
-                type="submit"
-              >
-                Register
-              </Button>
-              <div className="text-center">
-                {/* <a href="/signup">Existing user? Sign in</a> */}
-                <Link to="/login">Existing user? Sign in</Link>
-              </div>
-            </Form>
-          </Formbox>
-        </Container>
-        <ToastContainer autoClose={2000} />
-      </div>
-    );
-  }
-}
+            </FormGroup>
+            <FormGroup>
+              <Label>User ID</Label>
+              <Input
+                type="text"
+                id="userid"
+                name="userid"
+                placeholder="Enter username"
+                value={userid}
+                onChange={changeHandler}
+                required
+              />
+            </FormGroup>
+            {/* <FormGroup>
+          <Label>Email ID</Label>
+          <Input
+            type="email"
+            id="email"
+            placeholder="Enter email id"
+            value={email}
+            onChange={(e) => setemail(e.target.value)}
+            required
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label>Mobile number</Label>
+          <Input
+            type="number"
+            id="mobileNumber"
+            placeholder="Enter mobile number"
+            value={mobileNumber}
+            onChange={(e) => setmobileNumber(e.target.value)}
+            required
+          />
+        </FormGroup> */}
+            <FormGroup>
+              <Label>Password</Label>
+              <Input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Enter password"
+                value={password}
+                onChange={changeHandler}
+                required
+              />
+            </FormGroup>
+            <Button
+              className="btn-lg btn-dark btn-block"
+              id="submitButton"
+              type="submit"
+            >
+              Register
+            </Button>
+            <div className="text-center">
+              {/* <a href="/signup">Existing user? Sign in</a> */}
+              <Link to="/login">Existing user? Sign in</Link>
+            </div>
+          </Form>
+        </Formbox>
+      </Container>
+      <ToastContainer autoClose={2000} />
+    </div>
+  );
+};
+
+export default Signup;
