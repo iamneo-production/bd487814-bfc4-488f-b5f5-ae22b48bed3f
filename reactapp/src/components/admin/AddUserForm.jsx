c96d73d35c46ecb273a0ec0765c626125b178f4d
prabhathkumar1729 <prabhathkumar1729@gmail.com>
2022-04-01 00:46:23 +0530

    Fixed darkmode & added console resp in adduser pg

--- reactapp/src/components/admin/AddUserForm.jsx
+++ reactapp/src/components/admin/AddUserForm.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext,useState } from "react";
 import { Link } from "react-router-dom";
 import "./darkmode/dark.scss";
 import "./AddUserForm.css";
@@ -7,20 +7,41 @@ import { DarkModeContext } from "./darkmode/DarkModeContext";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer, toast } from "react-toastify";
 
-export default function AddUserForm() {
+export default function AddUserForm(props) {
   const notify = (e) => toast(e);
   const { darkMode } = useContext(DarkModeContext);
+  const [data, setData] = useState({
+    email: "",
+    username: "",
+    mobileNumber: "",
+    password: "",
+    role: "",
+  });
+
+  const { email, username, mobileNumber, password, role } = data;
+  const changeHandler = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(data);
+  }
+
   return (
     <div className={darkMode ? "app dark" : "app"}>
       <div className="newUser">
         <h1 className="newUserTitle">Add New User</h1>
-        <form className="newUserForm">
+        <form className="newUserForm" onSubmit={submitHandler}>
           <div className="newUserItem">
             <label>Username</label>
             <input
               type="text"
               placeholder="Enter Your Username"
               id="username"
+              name="username"
+              value={username}
+              onChange={changeHandler}
               required
             />
           </div>
@@ -30,6 +51,9 @@ export default function AddUserForm() {
               type="email"
               placeholder="Enter Your Email ID"
               id="email"
+              name="email"
+              value={email}
+              onChange={changeHandler}
               required
             />
           </div>
@@ -39,6 +63,9 @@ export default function AddUserForm() {
               type="password"
               placeholder="Enter Your Password"
               id="password"
+              name="password"
+              value={password}
+              onChange={changeHandler}
               required
             />
           </div>
@@ -48,6 +75,9 @@ export default function AddUserForm() {
               type="text"
               placeholder="Enter Your Phone Number"
               id="mobileNumber"
+              name="mobileNumber"
+              value={mobileNumber}
+              onChange={changeHandler}
               required
             />
           </div>
@@ -55,20 +85,21 @@ export default function AddUserForm() {
             <label>Role</label>
             <div>
               <span className="user_role">
-                <input type="radio" name="role" id="user" value="user" />
-                <label for="user">user</label>
+                <input type="radio" name="role" id="user" value="user" onChange={changeHandler} required/>
+                <label htmlFor="user">user</label>
               </span>
               <span className="user_role">
-                <input type="radio" name="role" id="admin" value="admin" />
-                <label for="admin">admin</label>
+                <input type="radio" name="role" id="admin" value="admin" onChange={changeHandler}required/>
+                <label htmlFor="admin">admin</label>
               </span>
             </div>
           </div>
           <button
             className="newUserButton"
-            onClick={notify("Added successfully")}
+            type="submit"
+            onClick={() => notify("Operation successfully")}
           >
-            Create
+            Submit
           </button>
         </form>
       </div>
